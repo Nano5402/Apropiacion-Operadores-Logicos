534e7685b31801cccdac9d10b6786b72f1ad7f80
Nano5402 <santiagocalvete69@gmail.com>
2025-10-06 23:21:34 -0500

    Realizo el ultimo punto bien terminado ahora solo falta comentar

---
+++ ejercicio3.js
@@ -0,0 +1,54 @@
+let mes = parseInt(prompt("Ingrese el número del mes que desee conocer"))
+
+switch (mes) {
+    case 1:
+    console.log("Enero");
+    alert("Enero")    
+        break;
+    case 2:
+    console.log("Febrero");
+    alert("Febrero")    
+        break;
+            case 3:
+    console.log("Marzo");
+    alert("Marzo")    
+        break;
+            case 4:
+    console.log("Abril (El mejor mes)");
+    alert("Abril (El mejor mes)")    
+        break;
+            case 5:
+    console.log("Mayo");
+    alert("Mayo")    
+        break;
+            case 6:
+    console.log("Junio");
+    alert("Junio")    
+        break;
+            case 7:
+    console.log("Julio");
+    alert("Julio")    
+        break;
+            case 8:
+    console.log("Agosto");
+    alert("Agosto")    
+        break;
+            case 9:
+    console.log("Septiembre");
+    alert("Septiembre")    
+        break;
+            case 10:
+    console.log("Octubre");
+    alert("Octubre")    
+        break;
+            case 11:
+    console.log("Noviembre");
+    alert("Noviembre")    
+        break;
+            case 12:
+    console.log("Diciembre");
+    alert("Diciembre")    
+        break;
+    default:
+        break;
+}
